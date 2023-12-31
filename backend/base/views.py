e698cfc233f693c592affd77892cef825e29107e
Nathaniel81 <nathanield356@gmail.com>
2023-12-31 09:52:59 +0300

    nc

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .products import products
-from rest_framework.generics import ListAPIView
+from rest_framework import generics
 from .serializers import ProductSerializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 def getRoutes(request):
     routes = [
 	]
     
     return JsonResponse(routes, safe=False)
-    
-class ProductList(ListAPIView):
+
+class ProductList(generics.ListAPIView):
     queryset = products
     serializer_class = ProductSerializers
-    
+
+class ProductDetail(generics.RetrieveAPIView):
+    queryset = products
+    serializer_class = ProductSerializers
+    lookup_field = '_id'
+    def get_queryset(self):
+        return products
