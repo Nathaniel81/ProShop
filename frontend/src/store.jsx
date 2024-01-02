1a2bd502052475fdd595c7f30c2d2f068a8494f1
Nathaniel81 <nathanield356@gmail.com>
2024-01-02 20:06:14 +0300

    nc

---
+++ frontend/src/store.jsx
@@ -0,0 +1,14 @@
+import { applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux';
+import { thunk } from 'redux-thunk';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import productListReducers from './reducers/ProductReducers';
+
+
+const reducer = combineReducers({
+	productList: productListReducers,
+});
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
