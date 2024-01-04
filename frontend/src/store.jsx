c3702e08e0083200c4e9f9c1359695de6a741436
Nathaniel81 <nathanield356@gmail.com>
2024-01-04 21:33:38 +0300

    nc

--- frontend/src/store.jsx
+++ frontend/src/store.jsx
@@ -2,14 +2,25 @@ import { applyMiddleware, combineReducers, legacy_createStore as createStore } f
 import { thunk } from 'redux-thunk';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import { productListReducers, productDetailReducers } from './reducers/ProductReducers';
-
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
 	productList: productListReducers,
-	productDetail: productDetailReducers
+	productDetail: productDetailReducers,
+	cart: cartReducer
 });
-const initialState = {};
+
 const middleware = [thunk];
+const cartItemsFromStorage = localStorage.getItem('cartItems') ?
+    JSON.parse(localStorage.getItem('cartItems')) : []
+const initialState = {
+	cart: {
+		cartItems: cartItemsFromStorage,
+		// shippingAddress: shippingAddressFromStorage,
+	}
+	// userLogin: { userInfo: userInfoFromStorage },
+}
 const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
 
 export default store;
