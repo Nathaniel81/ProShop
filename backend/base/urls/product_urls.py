de2ceaa702281e9e46bf2faaded974c49e91d0ac
Nathaniel81 <nathanield356@gmail.com>
2024-01-12 20:58:35 +0300

    nc

--- backend/base/urls/product_urls.py
+++ backend/base/urls/product_urls.py
@@ -4,5 +4,9 @@ from base.views import product_views as views
 
 urlpatterns = [
 	path('', views.ProductList.as_view(), name='products'),
-	path('detail/<str:_id>/', views.ProductDetail.as_view(), name='product-detail')
+  	path('create/', views.CreateProductView.as_view(), name="product-create"),
+	path('detail/<str:_id>/', views.ProductDetail.as_view(), name='product-detail'),
+ 
+	path('delete/<str:pk>/', views.DeleteProductView.as_view(), name="product-delete"),
+	path('update/<str:pk>/', views.UpdateProductView.as_view(), name="product-update"),
 ]
--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,9 +1,10 @@
-from django.http import JsonResponse
-from base.models import Product
 from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from base.models import Product
 from base.serializers import ProductSerializer
 # from django.contrib.auth.models import User
-# from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
 # from rest_framework.decorators import permission_classes
 
 
@@ -15,3 +16,65 @@ class ProductDetail(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = '_id'
+
+class DeleteProductView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAdminUser, IsAuthenticated]
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response('Product deleted')
+    
+class CreateProductView(generics.CreateAPIView):
+    # queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    def perform_create(self, serializer):
+        serializer.save(
+            user=self.request.user,
+            name='Sample Name',
+            price=0,
+            brand='Sample Brand',
+            countInStock=0,
+            category='Sample Category',
+            description=''
+        )
+
+# @api_view(['PUT'])
+# @permission_classes([IsAdminUser])
+# def updateProduct(request, pk):
+#     data = request.data
+#     product = Product.objects.get(_id=pk)
+
+#     product.name = data['name']
+#     product.price = data['price']
+#     product.brand = data['brand']
+#     product.countInStock = data['countInStock']
+#     product.category = data['category']
+#     product.description = data['description']
+
+#     product.save()
+
+#     serializer = ProductSerializer(product, many=False)
+#     return Response(serializer.data)
+
+class UpdateProductView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated, IsAdminUser]
+    
+    # def update(self, request, *args, **kwargs):
+    #     data = request.data
+    #     product = self.get_object()
+    #     product.name = data.get('name', product.name)
+    #     product.price = data.get('price', product.price)
+    #     product.brand = data.get('brand', product.brand)
+    #     product.countInStock = data.get('countInStock', product.countInStock)
+    #     product.category = data.get('category', product.category)
+    #     product.description = data.get('description', product.description)
+
+    #     product.save()
+
+    #     serializer = self.get_serializer(product)
+    #     return self.get_response(serializer.data)
